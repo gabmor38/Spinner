92c350187f3f977dc791b424a0b61be5eecfa0b5
gabmor38 <Gabriela.morenor@gmail.co>
2021-08-16 16:06:49 -0400

    modified to add comments

--- spinner2.js
+++ spinner2.js
@@ -3,9 +3,9 @@ const spinner = "|/-\\|/-\\|"; // declare the chars that need to spin"
 let timer = 100;
 
 for (let char of spinner) {
-  
+
   setTimeout( () => {
     process.stdout.write('\r' + char + "   ");
-  }, timer += 200);
+  }, timer += 200); //The 200 slows it down.
 
-};
+}
